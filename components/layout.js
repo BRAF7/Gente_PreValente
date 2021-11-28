27b1bffa8a5dd5fd1f4c7e5bc08e2f14242570ae
Brian <brianbtfa@hotmail.com>
2021-11-28 00:20:10 -0600

    navbar added

---
+++ components/layout.js
@@ -0,0 +1,6 @@
+//Importing styles
+import styles from './layout.module.css'
+
+export default function Layout({ children }) {
+  return <div className={styles.container}>{children}</div>
+}
--- pages/post/first_post.js
+++ pages/post/first_post.js
@@ -1,20 +1,33 @@
 import Link from "next/link";
 // Head is a react component
 import Head from "next/head";
+// we´re going to add some styles
+import Layout from "../../components/layout";
+//Importing NavBar
+import Navbar from "../../components/Navbar/Navbar";
 
 export default function FirstPost() {
   return (
     <>
-    {/* Were using this in order to give a tittle to the window */}
-      <Head>
+      {/* We're using this in order to give a tittle to the window */}
+      <Navbar></Navbar> 
+      <head>
         <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+      </head>
+      
+      <Layout>
+          
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+      </Layout>
     </>
   );
 }
